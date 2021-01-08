2bf8e0fc4d489e820277402d08d0351882a3068c
LorenzoPiombini <69109614+LorenzoPiombini@users.noreply.github.com>
2021-01-07 22:21:11 -0500

    Error handling done

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
