41f3c9ac7fefd0b55d9054ddea90dcf928447e40
LorenzoPiombini <69109614+LorenzoPiombini@users.noreply.github.com>
2021-01-09 07:55:57 -0500

    Angular basic setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  products: IProduct[] = [];
   title = 'BT English and More';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // tslint:disable-next-line: deprecation
+    this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination | any) => {
+    this.products = response.data;
+  }, error => {
+      console.log(error);
+    });
+  }
+
 }
